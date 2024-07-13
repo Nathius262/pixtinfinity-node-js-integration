d60f5e785b3483831f1d38a3899fb6b0c94e61b4
nathius262 <egbodonathaniel@yahoo.com>
2024-07-13 10:30:31 +0100

    modified server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-import express from "express";
-const router = express.Router();
+import express from 'express';
 import { engine } from 'express-handlebars';
+import fetch from 'node-fetch';
 import rootRouter from './routers/root.js';
-
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Derive the equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Set Handlebars as the template engine with .html extension
 
 app.engine('html', engine({ extname: '.html' }));
@@ -15,6 +19,7 @@ app.set('view engine', 'html');
 // Serve static files (CSS, JS, images)
 app.use(express.static('assets'));
 
+app.set('views', path.join(__dirname, 'views'));
 // routes
 app.use('/', rootRouter);
 //app.use('blog/', require('./routers/blog'));
