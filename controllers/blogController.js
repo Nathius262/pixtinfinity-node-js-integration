577d88c30f47fa790c453921f67602e881cfce4a
nathius262 <egbodonathaniel@yahoo.com>
2024-07-15 01:26:53 +0100

    modified blog page

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,12 +1,4 @@
 import fetch from 'node-fetch';
-import * as fs from 'fs'
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-
-// Derive the equivalent of __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const BLOG_API_URL = 'https://pixtinfinity.pythonanywhere.com/api';
 
@@ -15,8 +7,6 @@ const TAG_API_URL = `${BLOG_API_URL}/tag`
 const renderBlogPosts = async (req, res, viewTemplate) => {
     const page = req.query.page || 1; // Get the page number from the query parameters, default to 1 if not provided
     try {
-      const response = await fetch(`${BLOG_API_URL}/post?page=${page}`);
-      const data = await response.json();
 
       const [postsResponse, tagsResponse] = await Promise.all([
         fetch(`${BLOG_API_URL}/post?page=${page}`),
@@ -29,9 +19,11 @@ const renderBlogPosts = async (req, res, viewTemplate) => {
       const tags = tagsData.results;
 
       const blogs = postsData.results;
+      const firstFourBlogs = blogs.slice(0, 4);
+      const remainingBlogs = blogs.slice(4);
       const next = postsData.next;
       const previous = postsData.previous;
-      res.render(viewTemplate, { tags, blogs, next, previous, currentPage: page });
+      res.render(viewTemplate, { tags, blogs, firstFourBlogs, remainingBlogs, next, previous, currentPage: page });
     } catch (error) {
       console.error('Error fetching blog data:', error);
       res.status(500).send('Error fetching blog data');
